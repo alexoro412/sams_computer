e86b3d65424c6c69dd490cfb0caaac0c6bbe6081
Alex Oro <zyacir@gmail.com>
2018-02-05 22:10:26 -0700

    Add pretty colors, and warnings. Also some other fixes

--- assembler.hs
+++ assembler.hs
@@ -2,17 +2,19 @@
 import Data.Word
 import System.IO
 import System.Environment
-import Data.Map hiding (foldl, map, foldr)
+import Data.Map () -- hiding (foldl, map, foldr, filter)
 import qualified Data.Map as M
 import Data.Maybe (fromMaybe, listToMaybe)
 import Data.List (sortBy)
 import Data.Bits
 import Numeric (showHex)
 import System.Exit (exitFailure)
+import Control.Monad (when)
+import Data.Char (chr)
 
 type Address = String 
 
-data Expr = Let Address Word8 
+data Expr = Let Address Int 
     | Label Address 
     | Load Address 
     | Sub Address 
@@ -43,17 +45,17 @@ parse = _parse . words
 addresses :: [Word8]
 addresses = [minBound .. maxBound]
 
-varReduce :: [Expr] -> ([Expr], Map Address Word8)
+varReduce :: [Expr] -> ([Expr], M.Map Address Int)
 varReduce = foldr (\expr (exprs, bindings) -> case expr of 
-    Let a v -> (exprs, insert a v bindings)
-    e -> (e:exprs, bindings)) ([], empty)
+    Let a v -> (exprs, M.insert a v bindings)
+    e -> (e:exprs, bindings)) ([], M.empty)
 
-labelReduce :: Word8 -> [Expr] -> (Word8, [Expr], Map Address Word8)
+labelReduce :: Word8 -> [Expr] -> (Word8, [Expr], M.Map Address Word8)
 labelReduce offset = foldl (\(i, exprs, bindings) expr -> case expr of
-    Label a -> (i, exprs, insert a i bindings)
-    e -> (i+1, exprs ++ [e], bindings)) (offset, [], empty)
+    Label a -> (i, exprs, M.insert a i bindings)
+    e -> (i+1, exprs ++ [e], bindings)) (offset, [], M.empty)
 
-setAddresses :: [Expr] -> Map Address Word8 -> Map Address Word8 -> [CExpr]
+setAddresses :: [Expr] -> M.Map Address Word8 -> M.Map Address Word8 -> [CExpr]
 setAddresses exprs varBindings labelBindings = map fix exprs 
     where fix (Sub var) = CExpr CSub $ findVar var
           fix (Add var) = CExpr CAdd $ findVar var 
@@ -78,19 +80,29 @@ _opcode CHalt = 7
 opcode :: Op -> Word8 
 opcode e = shift (_opcode e) 5
 
+-- Makes sure no addresses go over 5 bits
 clamp :: Word8 -> Word8 
 clamp = min 31 
 
 assembleCExpr :: CExpr -> Word8 
+assembleCExpr (CExpr CLet x) = x
 assembleCExpr (CExpr e x) = opcode e .|. clamp x
 
 padZero :: String -> String 
 padZero [c] = '0':[c]
 padZero cs = cs
 
-safeHead :: [a] -> Maybe a
-safeHead (x:_) = Just x 
-safeHead [] = Nothing
+esc :: Char 
+esc = chr 27
+
+red :: String -> String 
+red s = esc:"[31m" ++ s ++ esc:"[0m"
+
+green :: String -> String
+green s = esc:"[32m" ++ s ++ esc:"[0m"
+
+bold :: String -> String 
+bold s = esc:"[1m" ++ s ++ esc:"[0m"
 
 main :: IO () 
 main = do
@@ -109,15 +121,21 @@ main = do
 
     hex <- withFile fname ReadMode (\handle -> do
         contents <- hGetContents handle
-        let lns = lines contents 
+        let lns = map unwords . filter (/= []) . map words $ lines contents 
             -- Remove variable declarations
-            (noVars, varValues) = varReduce $ map parse lns
-            -- Count number of variables
-            numVars = fromIntegral (length varValues)
+            (noVars, varValues_Int) = varReduce $ map parse lns
+            -- Convert [Int] to [Word8]
+            varValues = M.map fromIntegral varValues_Int
+
+        -- Warn of any variables that are too large
+        mapM_ (\(name, value) -> when (value > 31) $ putStrLn $ red "[Warning] " ++ "Var " ++ bold name ++ " was set to " ++ bold (show value) ++ ". It will be replaced with " ++ bold (show . clamp $ fromIntegral value)) $ M.toList varValues_Int
+       
+        let numVars = fromIntegral (length varValues)  -- Count number of variables
+        
             -- Assign an address to each variable
-            varAddresses = sortBy (\(_, n) (_, m) -> n `compare` m) $ zip (keys varValues) addresses
+            varAddresses = sortBy (\(_, n) (_, m) -> n `compare` m) $ zip (M.keys varValues) addresses
             -- Generate a map of variable name -> CAddress 
-            varBindings = fromList varAddresses
+            varBindings = M.fromList varAddresses
             -- Generate CExpr list with vars at their addresses
             varCode = map (\(name, _) -> CExpr CLet . fromMaybe (error "woah") $ M.lookup name varValues) varAddresses
 
@@ -128,12 +146,18 @@ main = do
             -- Assemble code into hex
             -- Strictness to allow file to be closed before next file written to
             !hex = map (padZero . (`showHex` "") . assembleCExpr) (varCode ++ withAddresses)
+
+        -- Warn when program is too long
+        when (length withAddresses > 256) $ do
+             putStrLn $ red "[Warning]" ++ " Program takes up too much space"
+             putStrLn $     "          Maximum space is " ++ bold "256" ++ " bytes"
+             putStrLn $     "          Program uses " ++ bold (show $ length withAddresses) ++ " bytes"
         return $ unwords hex)
 
     -- Write output
     writeFile output $ "v 2.1\n" ++ hex
 
-    -- YAY!
-    putStrLn $ "Output written to ./" ++ output
+    -- Say where it was written!
+    putStrLn . green $ "Output written to ./" ++ output
                 
     
